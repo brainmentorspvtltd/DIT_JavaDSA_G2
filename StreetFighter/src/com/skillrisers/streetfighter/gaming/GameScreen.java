cb3e8856ab1c95366202b1f71b862c5bd3f72170
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-03-28 17:47:08 +0530

    Add files via upload

---
+++ StreetFighter/src/com/skillrisers/streetfighter/gaming/GameScreen.java
@@ -0,0 +1,19 @@
+package com.skillrisers.streetfighter.gaming;
+
+import javax.swing.JFrame;
+
+public class GameScreen extends JFrame {
+	
+	public GameScreen() {
+		setSize(1500,800);
+		setTitle("Street Fighter By Brain Mentors");
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		GameScreen obj = new GameScreen();
+
+	}
+
+}
